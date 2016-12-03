a9cfe7f9a91f94e563a5ffc36326f363e986a295
Jeannie Yamakawa <jkyamakawa@gmail.com>
2016-12-02 20:45:38 -0600

    new socket chat messages appear on screen

--- client/js/controllers/gameController.js
+++ client/js/controllers/gameController.js
@@ -1,5 +1,17 @@
 app.controller('gameController', ['$scope', '$http', '$location', '$window',
   function($scope, $http, $location, $window) {
       $scope.message = 'controller is working'
+      $scope.messages = [];
+      $scope.submit = function(){
+      socket.emit('chat message', $scope.view.input);
+      $scope.view.input = '';
+      return false;
+    }
+    socket.on('chat message', function(msg){
+        console.log(msg, 'msg from socket in controller');
+        $scope.messages.push(msg)
+        console.log($scope.messages, 'scope.messages');
+    });
+
     }
 ]);
--- server/server.js
+++ server/server.js
@@ -41,7 +41,25 @@ app.get('/api', function(req, res) {
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
+
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+
 
 // Server Listener
 http.listen(port, function() {
