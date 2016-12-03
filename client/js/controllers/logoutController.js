58226700d60ead0d5d13eb7db809aa79d7476644
Tosin <midastouchproductions@gmail.com>
2016-12-03 12:32:53 -0600

    get logout to work get rid of extra ']' in citizen, add script tags to html

--- client/js/controllers/logoutController.js
+++ client/js/controllers/logoutController.js
@@ -1,3 +1,5 @@
 app.controller('logoutController', ['$scope', '$http', '$location', 'AuthFactory', '$localStorage', function($scope, $http, $location, AuthFactory, $localStorage) {
 
     $scope.logout = AuthFactory.Logout;
+    
+}]);
--- client/js/factories/Citizen.js
+++ client/js/factories/Citizen.js
@@ -1,16 +1,16 @@
 app.factory('citizenClass', function() {
-  return Citizen();
-}]);
+return Citizen();
+});
 
 function Citizen() {
 
-  this.awakeAtNight = false;
+    this.awakeAtNight = false;
 
-  // this.launchedCount = 0;
-  // this.launch = function() {
-  //   // Make a request to the remote API and include the apiToken
-  //   ...
-  //   this.launchedCount++;
-  // }
+    // this.launchedCount = 0;
+    // this.launch = function() {
+    //   // Make a request to the remote API and include the apiToken
+    //   ...
+    //   this.launchedCount++;
+    // }
 
 }
