6899b125fe67ddd22af3179b84e625838babf011
Jeannie Yamakawa <jkyamakawa@gmail.com>
2016-12-02 18:15:27 -0600

    config for ng-router

---
+++ client/js/app.js
@@ -0,0 +1,26 @@
+const app = angular.module("Werewolf", ["ngRoute"]);
+
+
+app.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'partials/home.html',
+      controller: 'homeController'
+    })
+    .when('/joingame', {
+      templateUrl: 'partials/joingame.html',
+      controller: 'joinGameController',
+    })
+    .when('/game', {
+        templateUrl: 'partials/game.html',
+        controller: 'gameController',
+    })
+    .when('signup', {
+        templateUrl: 'partials/signup.html',
+        controller: 'signupController',
+    })
+    .when('login', {
+        templateUrl: 'partials/login.html',
+        controller: 'loginController',
+    })
+});
