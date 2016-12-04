7109345abea7153a3f6a9274a0945c129a98a813
Jon West <jowest@corelogic.com>
2016-12-03 22:14:16 -0600

    Untested socket factory callback

--- client/js/controllers/waitingRoomController.js
+++ client/js/controllers/waitingRoomController.js
@@ -1,22 +1,23 @@
-app.controller('waitingRoomController', ['$scope', '$http', '$location', '$window', 'moderatorFactory', 'SocketFactory', '$routeParams',
-  function($scope, $http, $location, $window, moderatorFactory, SocketFactory, $routeParams) {
+app.controller('waitingRoomController',
+	['$scope', '$http', '$location', '$window', 'moderatorFactory', 'SocketFactory', '$routeParams',
+	 function ($scope, $http, $location, $window, moderatorFactory, SocketFactory, $routeParams) {
 
-    SocketFactory.jonDopeFunction;
+		 SocketFactory.onAddToWaitingRoom(function(data) {
+			 $scope.view.users = data;
+		 });
 
-    $scope.view = {};
+		 $scope.view = {};
 
+		 //When you join the waiting room you are pushed into the user list
+		 $scope.view.users = SocketFactory.getGameList()
 
+		 //Update the moderatorFactory everytime a new user is pushed into the list
+		 moderatorFactory.userList = $scope.view.users;
 
-    //When you join the waiting room you are pushed into the user list
-    $scope.view.users = SocketFactory.getGameList()
+		 $scope.view.userCount = $scope.view.users.length;
 
-    //Update the moderatorFactory everytime a new user is pushed into the list
-    moderatorFactory.userList = $scope.view.users;
-
-    $scope.view.userCount = $scope.view.users.length;
-
-    $scope.view.gameStart = function () {
-      moderatorFactory.start();
-    }
-  }
-]);
+		 $scope.view.gameStart = function () {
+			 moderatorFactory.start();
+		 }
+	 }
+	]);
