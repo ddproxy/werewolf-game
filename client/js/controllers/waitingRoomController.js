dc1b9670fa2f6849d511d5a44420db2d2407451b
Tosin <midastouchproductions@gmail.com>
2016-12-03 22:26:55 -0600

    wrapped the get list function in an apply

--- client/js/controllers/waitingRoomController.js
+++ client/js/controllers/waitingRoomController.js
@@ -3,15 +3,13 @@ app.controller('waitingRoomController',
 	 function ($scope, $http, $location, $window, moderatorFactory, SocketFactory, $routeParams) {
 
 		 SocketFactory.onAddToWaitingRoom(function(data) {
-       $apply(function(){
-         $scope.view.users = data;
-       })
+			 $scope.view.users = data;
 		 });
 
 		 $scope.view = {};
 
 		 //When you join the waiting room you are pushed into the user list
-		 $scope.view.users = SocketFactory.getGameList()
+		 $scope.view.users = $apply(SocketFactory.getGameList())
 
 		 //Update the moderatorFactory everytime a new user is pushed into the list
 		 moderatorFactory.userList = $scope.view.users;
