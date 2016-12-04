196f6a0dda2d997df72d92490e6343e426ac02af
Jon West <jowest@corelogic.com>
2016-12-04 15:33:17 -0600

    Check if the callback is set before using it

--- client/js/factories/Sockets.js
+++ client/js/factories/Sockets.js
@@ -4,21 +4,23 @@ app.factory('SocketFactory', function ($http, $localStorage, $routeParams) {
 
 	var cb = false;
 
+	socket.on('refreshWaitingRoom', function (data) {
+		console.log("refreshing the waiting room");
+		console.log(data);
+		gameList = data;
+		if (!cb) {
+			cb(gameList);
+		}
+	});
 
-    socket.on('refreshWaitingRoom', function(data) {
-        console.log("refreshing the waiting room");
-        console.log(data);
-        gameList = data;
-        cb(gameList);
-    })
 
-    obj.getGameList = function(callback) {
-        console.log("current game list");
-        console.log(gameList);
-        cb = callback;
-        callback(gameList);
-    }
+	obj.getGameList = function (callback) {
+		console.log("current game list");
+		console.log(gameList);
+		cb = callback;
+		callback(gameList);
+	}
 
-    return obj;
+	return obj;
 
 })
