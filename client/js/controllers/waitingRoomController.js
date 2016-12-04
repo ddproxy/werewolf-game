84d496ab697d1cce8d8d8fceceda0a2cef9078db
Tosin <midastouchproductions@gmail.com>
2016-12-03 22:31:04 -0600

    . was added

--- client/js/controllers/waitingRoomController.js
+++ client/js/controllers/waitingRoomController.js
@@ -9,7 +9,7 @@ app.controller('waitingRoomController',
 		 $scope.view = {};
 
 		 //When you join the waiting room you are pushed into the user list
-		 $scope.view.users = $apply(SocketFactory.getGameList())
+		 $scope.view.users = $scope.$apply(SocketFactory.getGameList())
 
 		 //Update the moderatorFactory everytime a new user is pushed into the list
 		 moderatorFactory.userList = $scope.view.users;
